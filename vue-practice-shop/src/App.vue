3bf7bd830a247a38ffe0ba7d04a51e8a1f5b558f
kamillesniak <kamillesniak02@gmail.com>
2019-01-21 00:14:23 +0100

    Product component

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7"></div>
+      <div class="row">
+        <div :key="product.id" class="col-md-6" v-for="product in products">
+          <product :product="product"></product>
+        </div>
+      </div>
+      <div class="col-md-5"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import products from "@/products.json";
+import Product from "@/components/Product.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Product
+  },
+  data() {
+    return {
+      products
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #fbf8f3;
 }
 </style>
+
+
