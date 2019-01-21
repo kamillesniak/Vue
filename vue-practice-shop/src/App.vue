6bd4c9d60947b79a25172f08ac67eaaa7d49cafa
kamillesniak <kamillesniak02@gmail.com>
2019-01-21 17:55:48 +0100

    Adding to card functionality

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -4,7 +4,7 @@
       <div class="col-md-7"></div>
       <div class="row">
         <div :key="product.id" class="col-md-6" v-for="product in products">
-          <product :product="product"></product>
+          <product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"></product>
         </div>
       </div>
       <div class="col-md-5"></div>
@@ -22,8 +22,22 @@ export default {
   },
   data() {
     return {
-      products
+      products,
+      cart: []
     };
+  },
+  methods: {
+    addToCart(product) {
+      this.cart.push(product);
+    },
+    isInCart(product){
+      const item = this.cart.find(item=> item.id === product.id);
+
+      if(item){
+        return true;
+      }
+      return false;
+    }
   }
 };
 </script>
